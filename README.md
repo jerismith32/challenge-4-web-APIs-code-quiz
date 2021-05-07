0194f16d7e5e52173354e73aa9438d2bf7488c3c
jerismith32 <jerismith32@gmail.com>
2021-05-07 14:39:59 -0400

    updated main page

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # challenge-4-web-APIs-code-quiz
+
+## Resources/References
+https://www.geeksforgeeks.org/how-to-hide-an-html-element-by-class-using-javascript/xs
---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//This is where the questions are housed with the choice options and the correct answer identified. 
+let questions = [
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correct: "A"
+    },
+    {
+
+    }
+]
